6c5dd42862c8604d4532f2e71d2ff7dd3c85c3b7
praisan <skylab135@gmail.com>
2024-09-19 14:48:19 +0700

    use cpu instead

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from transformers import BertTokenizer
 
 app = Flask(__name__)
 
-# Load the PyTorch model saved with joblib
+# Set the device to CUDA (GPU) if available, else use CPU
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = 'cpu'
+
+# Load the PyTorch model saved with joblib and move it to the device
 model = joblib.load('model/bert_text_classification_model.joblib')
+model.to(device)
 model.eval()  # Set the model to evaluation mode
 
 # Load the tokenizer
@@ -20,9 +25,7 @@ def transformerModel(text):
     # Tokenize input text using the same tokenizer used during training
     inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
 
-    # Move the input tensors to the same device as the model (GPU or CPU)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.to(device)
+    # Move the input tensors to the correct device (GPU or CPU)
     inputs = {key: value.to(device) for key, value in inputs.items()}
 
     # Run the model to get predictions
@@ -34,31 +37,31 @@ def transformerModel(text):
     predicted_class_id = logits.argmax(dim=1).item()
     predicted_label = id2label.get(predicted_class_id, 'Unknown')
 
-    # Return the prediction as a JSON response
     return predicted_label
 
 def blstm(text):
     return 9
+
 # API route to predict based on input text
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     text = data.get('text')
-    model = data.get('model')
+    model_type = data.get('model')
 
-    if not text or not model:
-        return jsonify({"error": "No text provided"}), 400
-    predicted_label = -1
-    if(model == 1):
+    if not text or model_type is None:
+        return jsonify({"error": "No text or model type provided"}), 400
+
+    # Based on the model type, choose the corresponding model
+    if model_type == 1:
         predicted_label = transformerModel(text)
     else:
         predicted_label = blstm(text)
 
-    # Return the prediction as a JSON response
     return jsonify({
         'text': text,
         'prediction': predicted_label,
-        'model':model
+        'model': model_type
     })
 
 # Run the Flask app
